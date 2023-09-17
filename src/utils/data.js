7200eb6a94ce09c04c76e7484b44f0047e1f9f27
Miracle <miraclepatidehen@gmail.com>
2023-09-17 01:21:09 +0100

    updated

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,3 +1,27 @@
+//images
+import deepEarth1 from '../assets/images/mobile/image-deep-earth.jpg'
+import deepEarth2 from '../assets/images/desktop/image-deep-earth.jpg'
+import nightArcade1 from '../assets/images/mobile/image-night-arcade.jpg'
+import nightArcade2 from '../assets/images/desktop/image-night-arcade.jpg'
+import soccer1 from '../assets/images/mobile/image-soccer-team.jpg'
+import soccer2 from '../assets/images/desktop/image-soccer-team.jpg'
+import theGrid1 from '../assets/images/mobile/image-grid.jpg'
+import theGrid2 from '../assets/images/desktop/image-grid.jpg'
+import aboveVR1 from '../assets/images/mobile/image-from-above.jpg'
+import aboveVR2 from '../assets/images/desktop/image-from-above.jpg'
+import pocket1 from '../assets/images/mobile/image-pocket-borealis.jpg'
+import pocket2 from '../assets/images/desktop/image-pocket-borealis.jpg'
+import curiosity1 from '../assets/images/mobile/image-curiosity.jpg'
+import curiosity2 from '../assets/images/desktop/image-curiosity.jpg'
+import fishEye1 from '../assets/images/mobile/image-fisheye.jpg'
+import fishEye2 from '../assets/images/desktop/image-fisheye.jpg'
+
+//icons
+import fbIcon from '../assets/images/icon-facebook.svg'
+import twIcon from '../assets/images/icon-twitter.svg'
+import pinIcon from '../assets/images/icon-pinterest.svg'
+import insIcon from '../assets/images/icon-instagram.svg'
+
 export const links = [
   { text: 'about', url: '#about' },
   { text: 'careers', url: '#' },
@@ -10,70 +34,70 @@ export const creations = [
   {
     text: 'deep earth',
     images: {
-      mobile: '/src/assets/images/mobile/image-deep-earth.jpg',
-      desktop: '/src/assets/images/desktop/image-deep-earth.jpg',
+      mobile: deepEarth1,
+      desktop: deepEarth2,
     },
   },
   {
     text: 'night arcade',
     images: {
-      mobile: '/src/assets/images/mobile/image-night-arcade.jpg',
-      desktop: '/src/assets/images/desktop/image-night-arcade.jpg',
+      mobile: nightArcade1,
+      desktop: nightArcade2,
     },
   },
   {
     text: 'soccer team vr',
     images: {
-      mobile: '/src/assets/images/mobile/image-soccer-team.jpg',
-      desktop: '/src/assets/images/desktop/image-soccer-team.jpg',
+      mobile: soccer1,
+      desktop: soccer2,
     },
   },
   {
     text: 'the grid',
     images: {
-      mobile: '/src/assets/images/mobile/image-grid.jpg',
-      desktop: '/src/assets/images/desktop/image-grid.jpg',
+      mobile: theGrid1,
+      desktop: theGrid2,
     },
   },
   {
     text: 'from up above vr',
     images: {
-      mobile: '/src/assets/images/mobile/image-from-above.jpg',
-      desktop: '/src/assets/images/desktop/image-from-above.jpg',
+      mobile: aboveVR1,
+      desktop: aboveVR2,
     },
   },
   {
     text: 'pocket borealis',
     images: {
-      mobile: '/src/assets/images/mobile/image-pocket-borealis.jpg',
-      desktop: '/src/assets/images/desktop/image-pocket-borealis.jpg',
+      mobile: pocket1,
+      desktop: pocket2,
     },
   },
   {
     text: 'the curiosity',
     images: {
-      mobile: '/src/assets/images/mobile/image-curiosity.jpg',
-      desktop: '/src/assets/images/desktop/image-curiosity.jpg',
+      mobile: curiosity1,
+      desktop: curiosity2,
     },
   },
   {
     text: 'make it fish eye',
     images: {
-      mobile: '/src/assets/images/mobile/image-fisheye.jpg',
-      desktop: '/src/assets/images/desktop/image-fisheye.jpg',
+      mobile: fishEye1,
+      desktop: fishEye2,
     },
   },
 ]
 
 export const socialLinks = [
-  { icon: '/src/assets/images/icon-facebook.svg', url: '/' },
+  { icon: fbIcon, url: '/' },
   {
-    icon: '/src/assets/images/icon-twitter.svg',
+    icon: twIcon,
     url: 'https://twitter.com/mimiDevs',
   },
-  { icon: '/src/assets/images/icon-pinterest.svg', url: '/' },
+  { icon: pinIcon, url: '/' },
   {
-    icon: '/src/assets/images/icon-instagram.svg',
+    icon: insIcon,
     url: 'https://www.instagram.com/pi_miracle',
   },
 ]
